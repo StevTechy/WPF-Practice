bdfdfeb4a739561a6afbfd41a11254304755a14f
Steven Hooper <stevenms@outlook.com>
2017-02-04 18:49:16 +0000

    Added use of value converter to correctly format DateTime values on data binding

--- WPF Practice/Employee.cs
+++ WPF Practice/Employee.cs
@@ -1,10 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.ComponentModel;
-using System.Linq;
 using System.Runtime.CompilerServices;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace WPF_Practice
 {
@@ -12,9 +9,11 @@ namespace WPF_Practice
     {
         private string name;
         private string title;
+        private DateTime startDate;
 
         public string Name { get { return name; } set { name = value; OnPropertyChanged(); } }
         public string Title { get { return title; } set { title = value; OnPropertyChanged(); } }
+        public DateTime StartDate { get { return startDate; } set { startDate = value; OnPropertyChanged(); } }
 
         public event PropertyChangedEventHandler PropertyChanged;
 
@@ -25,6 +24,20 @@ namespace WPF_Practice
                 Name = "Steven",
                 Title = "Application Developer"
             };
+        }
+
+        public static ObservableCollection<Employee> GetEmployees()
+        {
+            //By using ObservableCollection, view is notified on additions or deletions to collection
+            var employees = new ObservableCollection<Employee>();
+            employees.Add(new Employee() { Name = "Barbara", Title = "Cleaner", StartDate = new DateTime(2004,10,10) });
+            employees.Add(new Employee() { Name = "Daniel", Title = "Developer", StartDate = new DateTime(2005, 4, 5) });
+            employees.Add(new Employee() { Name = "Jeff", Title = "Solutions Analyst", StartDate = new DateTime(2006, 10, 4) });
+            employees.Add(new Employee() { Name = "Gregory", Title = "Project Manager", StartDate = new DateTime(2007, 3, 2) });
+            employees.Add(new Employee() { Name = "Jennifer", Title = "HR Advisor", StartDate = new DateTime(2008, 10, 12) });
+            employees.Add(new Employee() { Name = "James", Title = "Reporting Analyst", StartDate = new DateTime(2009, 10, 16) });
+            employees.Add(new Employee() { Name = "Alec", Title = "Scrum Master", StartDate = new DateTime(2010, 12, 19) });
+            return employees;
         }
 
         private void OnPropertyChanged([CallerMemberName] string caller = "")
