aca6ac61e82a59a3f55946f778f123e47c5c1ee0
Steven Hooper <stevenms@outlook.com>
2017-02-04 16:50:22 +0000

    Added event to BasicForm and data extraction from UI components

--- WPF Practice/BasicForm.xaml.cs
+++ WPF Practice/BasicForm.xaml.cs
@@ -24,5 +24,47 @@ namespace WPF_Practice
         {
             InitializeComponent();
         }
+
+        private void btnSave_Click(object sender, RoutedEventArgs e)
+        {
+            var user = new User();
+
+            user.FullName = txbFullName.Text;
+
+            if (rdoSexMale.IsChecked != null)
+                if (Convert.ToBoolean(rdoSexMale.IsChecked))
+                    user.Sex = "Male";
+            if (rdoSexFemale.IsChecked != null)
+                if (Convert.ToBoolean(rdoSexFemale.IsChecked))
+                    user.Sex = "Female";
+
+            user.Machines = new List<string>();
+
+            if (chkDesktop.IsChecked != null)
+                if (Convert.ToBoolean(chkDesktop.IsChecked))
+                    user.Machines.Add("Desktop");
+            if (chkLaptop.IsChecked != null)
+                if (Convert.ToBoolean(chkLaptop.IsChecked))
+                    user.Machines.Add("Laptop");
+
+            if (cmbJob.SelectedItem != null)
+                user.Job = cmbJob.SelectedItem.ToString();
+
+            user.PromisedDeliveryDate = clnPromisedDeliveryDate.SelectedDate;
+
+            MessageBox.Show($"Full Name: {user.FullName}, Sex: {user.Sex}, Machines: {string.Join(",", user.Machines)}," +
+                $" Job: {user.Job}, Promised Delivery Date: {user.PromisedDeliveryDate}");
+        }
+
+        internal class User
+        {
+            public string FullName { get; set; }
+            public string Sex { get; set; }
+            public ICollection<string> Machines { get; set; }
+            public string Job { get; set; }
+            public DateTime? PromisedDeliveryDate { get; set; }
+        }
     }
+
+
 }
