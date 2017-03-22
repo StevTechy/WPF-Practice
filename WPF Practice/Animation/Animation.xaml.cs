e545592710e869d2f6d04e96c24f241c7ef17190
Steven Hooper <stevenms@outlook.com>
2017-03-22 22:00:19 +0000

    Testing animation using Blend

---
+++ WPF Practice/Animation/Animation.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WPF_Practice.Animation
+{
+    /// <summary>
+    /// Interaction logic for Animation.xaml
+    /// </summary>
+    public partial class Animation : Window
+    {
+        public Animation()
+        {
+            InitializeComponent();
+
+            DoubleAnimation animation = new DoubleAnimation();
+            animation.From = 0;
+            animation.To = 200;
+            //TimeSpan.Parse (hours:minutes:seconds)
+            animation.Duration = new Duration(TimeSpan.Parse("0:0:5"));
+            animation.AutoReverse = true;
+            animation.RepeatBehavior = new RepeatBehavior(TimeSpan.Parse("0:0:20"));
+            ellipse.BeginAnimation(System.Windows.Controls.Canvas.LeftProperty, animation);
+        }
+    }
+}
